952e94445914ea53f24cf1afce6d7a9e74dab0d2
Ivan Herrera Barrios <ivanherrerabarrios16@gmail.com>
2024-02-02 19:15:37 -0600

    Cycles

---
+++ app.js
@@ -0,0 +1,18 @@
+/**
+ * while (<condition>) {
+ * This code repeats as long as the condition is met
+ * }
+ */
+let i = 0; //initiator
+while (i < 10) { //condition
+    console.log("Hello World");
+    i++; //i = i + 1;
+}
+
+/*
+ *infinite cycle
+ for (;;) 
+ */
+for (let i = 0; i < 10; i++) {
+    console.log("Hello World");   
+}
